c847eacb7144b3cb969c6209fbb4b95aa57d4eab
wukaoyu <317265524@qq.com>
2022-07-21 23:45:24 +0800

    feat:完成按钮组件练习

--- src/views/antd/commonly.vue
+++ src/views/antd/commonly.vue
@@ -7,21 +7,27 @@
         <a-button @click="changeSize('large')">大</a-button>
         <a-button @click="changeSize('default')">中</a-button>
         <a-button @click="changeSize('small')">小</a-button>
-        <a-button @click="changeDanger()">危险按钮</a-button>
+        <a-button @click="changeDanger">危险按钮</a-button>
+        <a-button @click="changeDisable">禁用按钮</a-button>
+        <a-button @click="changeShape('default')" shape="default">默认形状</a-button>
+        <a-button @click="changeShape('circle')" shape="circle">
+          <template #icon><SearchOutlined /></template>
+        </a-button>
+        <a-button @click="changeShape('round')" shape="round">圆角</a-button>
       </div>
       <div class="content">
         <p class="title">不同type</p>
-        <a-button type="primary" :size="btnSize" :danger="btnDanger" :loading="isLoading">重要按钮（type:primary）</a-button>
-        <a-button type="dashed" :size="btnSize" :danger="btnDanger" :loading="isLoading">虚线按钮（type:dashed）</a-button>
-        <a-button type="text" :size="btnSize" :danger="btnDanger" :loading="isLoading">文字按钮（type:text）</a-button>
-        <a-button type="link" :size="btnSize" :danger="btnDanger" :loading="isLoading">链接按钮（type:link）</a-button>
-        <a-button :size="btnSize" :danger="btnDanger" :loading="isLoading">默认按钮</a-button>
+        <a-button type="primary"  :size="btnSize" :shape="btnShape" :danger="btnDanger" :loading="isLoading" :disabled="isDisabled">重要按钮（type:primary）</a-button>
+        <a-button type="dashed" :size="btnSize" :shape="btnShape" :danger="btnDanger" :loading="isLoading" :disabled="isDisabled">虚线按钮（type:dashed）</a-button>
+        <a-button type="text" :size="btnSize" :shape="btnShape" :danger="btnDanger" :loading="isLoading" :disabled="isDisabled">文字按钮（type:text）</a-button>
+        <a-button type="link" :size="btnSize" :shape="btnShape" :danger="btnDanger" :loading="isLoading" :disabled="isDisabled">链接按钮（type:link）</a-button>
+        <a-button :shape="btnShape" :size="btnSize" :danger="btnDanger" :loading="isLoading" :disabled="isDisabled">默认按钮</a-button>
       </div>
       <div class="content" :style="{ background: 'rgb(190, 200, 200)', padding: '26px 16px 16px' }">
         <p class="title">幽灵模式ghost</p>
-        <a-button ghost type="primary" :size="btnSize" :danger="btnDanger" :loading="isLoading">重要按钮（type:primary）</a-button>
-        <a-button ghost type="dashed" :size="btnSize" :danger="btnDanger" :loading="isLoading">虚线按钮（type:dashed）</a-button>
-        <a-button ghost :size="btnSize" :danger="btnDanger" :loading="isLoading">默认按钮</a-button>
+        <a-button ghost type="primary" :size="btnSize" :shape="btnShape" :danger="btnDanger" :loading="isLoading" :disabled="isDisabled">重要按钮（type:primary）</a-button>
+        <a-button ghost type="dashed" :size="btnSize" :shape="btnShape" :danger="btnDanger" :loading="isLoading" :disabled="isDisabled">虚线按钮（type:dashed）</a-button>
+        <a-button ghost :size="btnSize" :danger="btnDanger" :shape="btnShape" :loading="isLoading" :disabled="isDisabled">默认按钮</a-button>
       </div>
     </a-card>
   </div>
@@ -29,9 +35,12 @@
 
 <script setup lang='ts'>
 import { ref } from 'vue'
+import { SearchOutlined } from '@ant-design/icons-vue';
 const btnSize = ref('default')
 const isLoading = ref(false)
 const btnDanger = ref(false)
+const isDisabled = ref(false)
+const btnShape = ref('default')
 
 // 更改按钮loading状态
 const changeLoading = () => {
@@ -45,6 +54,16 @@ const changeSize = (type: string) => {
 // 更改按钮危险状态
 const changeDanger = () => {
   btnDanger.value = !btnDanger.value
+}
+
+// 更改disable状态
+const changeDisable = () => {
+  isDisabled.value = !isDisabled.value
+}
+
+// 更改按钮形状
+const changeShape = (type: string) => {
+  btnShape.value = type
 }
 </script>
 
