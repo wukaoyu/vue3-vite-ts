e807e2a3470be304160fa8ba28bb8d0474d7a600
wuky <wuky>
2022-07-25 18:03:53 +0800

    feat:input练习

--- src/views/antd/commonly.vue
+++ src/views/antd/commonly.vue
@@ -32,16 +32,41 @@
     </a-card>
     <a-card class="card" title="输入框" hoverable>
       <a-space direction="vertical" style="width: 100%">
-        <a-input class="card-item" :value="inputValue" placeholder="placeholder"/>
-        <a-textarea class="card-item" :value="inputValue" placeholder="textarea的input" :auto-size="{minRows: 2, maxRows: 5}"/>
-        <a-input-search :value="inputValue" placeholder="input search text"  @search="btnSearch"/>
-        <a-input-search :value="inputValue" enter-button placeholder="设置了enter-button"  @search="btnSearch"/>
-        <a-input-search :value="inputValue" enter-button="Search" placeholder="enter-button赋值按钮会变成文字" @search="btnSearch"/>
-        <a-input-search :value="inputValue" placeholder="自定义搜索按钮" @search="btnSearch">
+        <a-input class="card-item" v-model:value="inputValue" placeholder="placeholder"/>
+        <a-textarea class="card-item" v-model:value="inputValue" placeholder="textarea的input" :auto-size="{minRows: 2, maxRows: 5}"/>
+        <a-input-search v-model:value="inputValue" placeholder="input search text"  @search="btnSearch"/>
+        <a-input-search v-model:value="inputValue" enter-button placeholder="设置了enter-button"  @search="btnSearch"/>
+        <a-input-search v-model:value="inputValue" enter-button="Search" placeholder="enter-button赋值按钮会变成文字" @search="btnSearch"/>
+        <a-input-search v-model:value="inputValue" placeholder="自定义搜索按钮" @search="btnSearch">
             <template #enterButton>
               <a-button>Custom</a-button>
             </template>
         </a-input-search>
+        <a-input v-model:value="inputValue" addon-before="Http://" addon-after=".com" placeholder="配置固定组合"/>
+        <a-input v-model:value="inputValue">
+          <template #addonBefore>
+            <a-select v-model:value="inputBeforeValue">
+              <a-select-option value="Http://">Http://</a-select-option>
+              <a-select-option value="Https://">Https://</a-select-option>
+            </a-select>
+          </template>
+          <template #addonAfter>
+            <a-select v-model:value="inputAfterValue">
+              <a-select-option value=".com/">.com</a-select-option>
+              <a-select-option value=".jp/">.jp</a-select-option>
+              <a-select-option value=".cn/">.cn</a-select-option>
+              <a-select-option value=".org/">.org</a-select-option>
+            </a-select>
+          </template>
+        </a-input>
+        <a-input v-model:value="inputValue" show-count :maxlength="20" placeholder="显示文字数量/上限"/>
+        <a-input-password v-model:value="inputValue" placeholder="密码框"/>
+        <a-input v-model:value="inputValue" allow-clear placeholder="icon清除输入框内容"/>
+      </a-space>
+    </a-card>
+    <a-card>
+      <a-space>
+        
       </a-space>
     </a-card>
   </div>
@@ -58,6 +83,8 @@ const isDisabled = ref(false)
 const btnShape = ref('default')
 
 const inputValue = ref('')
+const inputBeforeValue = ref('Http://')
+const inputAfterValue = ref('.com')
 
 // 更改按钮loading状态
 const changeLoading = () => {
@@ -84,9 +111,10 @@ const changeShape = (type: string) => {
 }
 
 // input-search点击事件
-const btnSearch = () => [
+const btnSearch = () => {
   console.log('搜索按钮点击')
-]
+  console.log(inputValue.value)
+}
 
 </script>
 
